131a17b0ca1eaba4bdf5ddf119f73f5d3876d8bf
ZeynepOzan <ozanzzeynep@gmail.com>
2023-11-02 00:44:48 +0300

    spring homework added

--- src/main/java/com/tobeto/Lesson_01_11/SpringApplication.java
+++ src/main/java/com/tobeto/Lesson_01_11/SpringApplication.java
@@ -1,13 +1,12 @@
 package com.tobeto.Lesson_01_11;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Lesson0111Application {
+public class SpringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Lesson0111Application.class, args);
+		org.springframework.boot.SpringApplication.run(SpringApplication.class, args);
 	}
 
 }
